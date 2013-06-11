c2a439f6871c1057d453b22683c4217a4e3ba7a5
Florian Kelbert <kelbert@cs.tum.edu>
2013-06-11 18:35:35 +0200

    added two syscalls (*process_vm_write)

--- plain/src/ptrace/syscalls.h
+++ plain/src/ptrace/syscalls.h
@@ -10,25 +10,25 @@
 
 #include <syscall.h>
 
-#define SOCKET_OFFSET 347
+#define SOCKET_OFFSET 349
 
-#define SYS_socket 348
-#define SYS_bind 349
-#define SYS_connect 350
-#define SYS_listen 351
-#define SYS_accept 352
-#define SYS_getsockname 353
-#define SYS_getpeername 354
-#define SYS_socketpair 355
-#define SYS_send 356
-#define SYS_recv 357
-#define SYS_sendto 358
-#define SYS_recvfrom 359
-#define SYS_shutdown 360
-#define SYS_setsockopt 361
-#define SYS_getsockopt 362
-#define SYS_sendmsg 363
-#define SYS_recvmsg 364
+#define SYS_socket 350
+#define SYS_bind 351
+#define SYS_connect 352
+#define SYS_listen 353
+#define SYS_accept 354
+#define SYS_getsockname 355
+#define SYS_getpeername 356
+#define SYS_socketpair 357
+#define SYS_send 358
+#define SYS_recv 359
+#define SYS_sendto 360
+#define SYS_recvfrom 361
+#define SYS_shutdown 362
+#define SYS_setsockopt 363
+#define SYS_getsockopt 364
+#define SYS_sendmsg 365
+#define SYS_recvmsg 366
 
 #define INTERCEPT_EXECVE 1
 #define INTERCEPT_SOCKETCALL 1
