61d3a1ce349a3c456d33d9bb935131263b536321
Florian Kelbert <kelbert@cs.tum.edu>
2013-07-02 03:01:34 +0200

    Some cleanups and improved splus()

--- ucDeclass.h
+++ ucDeclass.h
@@ -16,14 +16,6 @@
 #include "ucSyscall.h"
 #include "ucPIP.h"
 
-//extern int ucDeclassEvents[];
-//
-//struct declass {
-//		pid_t pid;
-//		struct timeval time;
-//		int syscallno;
-//		char *data;
-//};
 
 #define ucDeclass_errorExit(msg) \
 			fprintf(stderr, "%s\n", msg); \
@@ -33,7 +25,5 @@
 
 
 void ucDeclass__init();
-
-void ucDeclassEvent(pid_t pid, char scno);
 
 #endif /* UC_DECLASS_H_ */
