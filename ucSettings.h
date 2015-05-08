5201ed85e73a4d42d852bb9359d3704dd622f33c
Florian Kelbert <kelbert@in.tum.de>
2015-05-08 08:55:48 +0200

    Add option to make default strace silent (STRACE_SILENT 1)

--- ucSettings.h
+++ ucSettings.h
@@ -1,19 +1,21 @@
 #ifndef UC_SETTINGS_H
 #define UC_SETTINGS_H
 
-#define UC_ENABLED 1
+#define STRACE_SILENT 1 // Make default strace silent (only effective if UC_ENABLED 0)
+
+#define UC_ENABLED 0
 
 #define UC_DEBUG 0
 #define UC_LOG_EVENT_PARAMETERS 0
-#define UC_LOG_TIME 0
+#define UC_LOG_TIME 1
 
 // Enable _either_ of the two following options
 #define UC_JNI 0
 #define UC_THRIFT 1
 
-#define UC_ONLY_EXECVE 0
+#define UC_ONLY_EXECVE 1
 
-#define UC_THRIFT_SSL_ENABLED 0
+#define UC_THRIFT_SSL_ENABLED 1
 #define UC_THRIFT_PDP_PORT 21003
 
 #define USER_CLASSPATH "Jni-1.0-jar-with-dependencies.jar"
